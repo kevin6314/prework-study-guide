31d59b394a45e96bbe71cc5e687011223fb3f134
limrod631 <kevin.omwega@gmail.com>
2023-09-12 12:25:59 -0500

    add JS code

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var topics = ["HTML", "CSS", "GIT", "JavaScript"];
+
+for(var x = 0; x<topics.length; x++){
+console.log(topics[x]);
+}
+
+/* var topic = "HTML";
+
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
+  */
+
+  //set array with acceptable values
+  //assign user input
+  //check that user input is acceptable
+  //print outcome
